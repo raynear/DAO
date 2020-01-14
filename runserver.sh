2dd3ad95f64f5e671c503975d73a7b621d0b3786
Ubuntu <ubuntu@ip-172-31-18-23.ap-northeast-2.compute.internal>
2020-01-14 05:13:43 +0000

    deployed SCORE, https => http(tbears cannot run as https

--- runserver.sh
+++ runserver.sh
@@ -4,5 +4,5 @@ PROJECT_DIR=$(cd "$(dirname "$0")" && pwd)
 cd $PROJECT_DIR
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runsslserver 0.0.0.0:8080 --certificate django.cert --key django.key
+python manage.py runserver 0.0.0.0:8080
 
