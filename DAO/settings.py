9de94cca77f6f54d1d4466677b3e27fae7e20dc5
raynear <raynear@gmail.com>
2020-01-29 16:51:36 +0900

    add static folder

--- DAO/settings.py
+++ DAO/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(
-    BASE_DIR, 'reactapp', 'build', 'static'), os.path.join(BASE_DIR, 'DAO', 'static')]
+    BASE_DIR, 'reactapp', 'build', 'static'), os.path.join(BASE_DIR, 'DAO', 'static'), os.path.join(BASE_DIR, 'reactapp', 'public')]
 
 
 # auth
