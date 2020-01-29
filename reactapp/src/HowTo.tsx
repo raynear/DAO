8625e3be155eb9b6a9462e9fc17b37f671768df3
raynear <raynear@gmail.com>
2020-01-29 16:05:56 +0900

    edit Howto

--- reactapp/src/HowTo.tsx
+++ reactapp/src/HowTo.tsx
@@ -14,22 +14,22 @@ function HowTo(props: any) {
 Sign up
 
 1. Click “Join Now” on the top right to sign up
-[https://lh5.googleusercontent.com/GXaCwe0N9-u6jfGLp1QNjAtbFstvihaBK-qeo9PYR_dJmoEfEoyq1IPeaWNPcLekl47qgZdeMar7iKbrpHsB0I25m4qcvTrhR2jczfNPz1U0kKylqI9hYR1c14zd9ALwwKYwiova](https://lh5.googleusercontent.com/GXaCwe0N9-u6jfGLp1QNjAtbFstvihaBK-qeo9PYR_dJmoEfEoyq1IPeaWNPcLekl47qgZdeMar7iKbrpHsB0I25m4qcvTrhR2jczfNPz1U0kKylqI9hYR1c14zd9ALwwKYwiova)
+[https://icon.vote/static/img1.png](https://icon.vote/static/img1.png)
 2. Type in “Username” and “Password” to sign up
 3. Verify P-rep node ownership by signing a transaction from publicly known P-rep wallet
     1. Click “Verify ICON Address” to sign a transaction
     2. Click “Check Your Verified Address” so that blockchain can confirm
-[https://lh5.googleusercontent.com/d-DtJB4tbztPgrotVcNn1XCnziLblW8MNMA-UKZPDArJ6WUog6MjUXNyJZptdu6o2dGIVASI8CgutOBd20XpS5rXgirFLFgv_9GRR_liap6naniiFdmSYlXbQG4GRHaFUyF7tO5P](https://lh5.googleusercontent.com/d-DtJB4tbztPgrotVcNn1XCnziLblW8MNMA-UKZPDArJ6WUog6MjUXNyJZptdu6o2dGIVASI8CgutOBd20XpS5rXgirFLFgv_9GRR_liap6naniiFdmSYlXbQG4GRHaFUyF7tO5P)
+[https://icon.vote/static/img2.png](https://icon.vote/static/img2.png)
 4. Blockchain has verified the P-rep ownership. The following image will appear
     1. Click “Make New P-rep DAO Page” to build your own DAO page
-[https://lh3.googleusercontent.com/1OLzmLPzrVPzlUb4z79QOl7fLOdzMU-IoEJBAbqOQx60BBJCsJq97vNOZ-V9KmYWV02cIz6ZOLffVnotIHZtrgJXyC0DT9p57pJRBbrph3MjKTYzLJDlTAEww8UGyIyYRyFM5rPk](https://lh3.googleusercontent.com/1OLzmLPzrVPzlUb4z79QOl7fLOdzMU-IoEJBAbqOQx60BBJCsJq97vNOZ-V9KmYWV02cIz6ZOLffVnotIHZtrgJXyC0DT9p57pJRBbrph3MjKTYzLJDlTAEww8UGyIyYRyFM5rPk)
+[https://icon.vote/static/img3.png](https://icon.vote/static/img3.png)
 
 Propose a vote
 
 1. Sign in as a P-rep with “Username” and “Password”
 2. “Launch new votes” will appear for P-reps
 3. Click “Launch new votes”
-[https://lh3.googleusercontent.com/lw8Moqqz0zCU9VAEEJCQZujoc-K9BEYlbZBAyA13hTdbBX52H-jMY9mBjljQy-m-uSFfrafYXSucrqmWJBG7L5lzevCRKW9X6xrhSeKMNLPhEOZeag_S27dPth9nh-DJ3bPpx7CH](https://lh3.googleusercontent.com/lw8Moqqz0zCU9VAEEJCQZujoc-K9BEYlbZBAyA13hTdbBX52H-jMY9mBjljQy-m-uSFfrafYXSucrqmWJBG7L5lzevCRKW9X6xrhSeKMNLPhEOZeag_S27dPth9nh-DJ3bPpx7CH)
+[https://icon.vote/static/img4.png](https://icon.vote/static/img4.png)
 4. Click the “NEW” button to propose a vote
 5. Fill out the following information
     1. Subject of the vote
@@ -38,15 +38,15 @@ Propose a vote
     4. Vote Duration
     5. Participation Quorum (ex: vote needs 20%+ of ICX delegated to ICX Station to participate in the vote for the vote to be valid)
     6. Minimum approval rate to pass (ex: option A needs 50%+ of votes to approve in order to pass)
-[https://lh4.googleusercontent.com/pAhIKR5MOb1aQ05vAWvyADFl0Aw0V7Slj9XLhoD2uZj_AJcEgE7sNcQKQGqPTFRcjVtPey2gn3pcjIqd0ZWR5ATq1BAa2X3g64hlOxAjlGjxs2cA4rMXbziavqQXyt6Qy2Zm3Em5](https://lh4.googleusercontent.com/pAhIKR5MOb1aQ05vAWvyADFl0Aw0V7Slj9XLhoD2uZj_AJcEgE7sNcQKQGqPTFRcjVtPey2gn3pcjIqd0ZWR5ATq1BAa2X3g64hlOxAjlGjxs2cA4rMXbziavqQXyt6Qy2Zm3Em5)
+[https://icon.vote/static/img5.png](https://icon.vote/static/img5.png)
 6. The Draft page will appear. Please make sure to check the details and click “Publish”
     1. Green graph shows “Quorum requirement”
     2. Blue graph shows “Minimum Approval Rate”
     3. “Total Delegation” represents Total # of ICX delegated to the P-rep
-[https://lh6.googleusercontent.com/vX2LN9M6FnNlqRw6LZKa4oaFLXiVW9qEc9F09I_ZUtw8PGBJf6nNsWQPkOnHa4FP1LX6XvSskNDLQDv4mgaoLt5tI5j6YXqmk-tswWaAUAncDG-HdG0BFj-6DR7mrjze_qOQvs0G](https://lh6.googleusercontent.com/vX2LN9M6FnNlqRw6LZKa4oaFLXiVW9qEc9F09I_ZUtw8PGBJf6nNsWQPkOnHa4FP1LX6XvSskNDLQDv4mgaoLt5tI5j6YXqmk-tswWaAUAncDG-HdG0BFj-6DR7mrjze_qOQvs0G)
+[https://icon.vote/static/img6.png](https://icon.vote/static/img6.png)
 7. The new vote will be recorded on the blockchain and the transaction can be found in the vote explorer under the P-rep’s page
 8. Make sure to share the vote link to your community so that they can participate in the vote (Twitter, FB link available)
-[https://lh5.googleusercontent.com/cjU68W6OdEgoT5OBm_N7okrAXwgmOVcq-1Zcy3B_IW5JSDaupkNTaM1Lvg1XKhg2hiU05BX0SjX0WVPezolCJQaX6zAFWZpZkXSXcL5mDi30SwqYd12tyXgP9gsGeEFuxxX26XCT](https://lh5.googleusercontent.com/cjU68W6OdEgoT5OBm_N7okrAXwgmOVcq-1Zcy3B_IW5JSDaupkNTaM1Lvg1XKhg2hiU05BX0SjX0WVPezolCJQaX6zAFWZpZkXSXcL5mDi30SwqYd12tyXgP9gsGeEFuxxX26XCT)
+[https://icon.vote/static/img7.png](https://icon.vote/static/img7.png)
 
 **Instructions for ICX delegator**
 
@@ -55,15 +55,15 @@ Propose a vote
 Sign up
 
 1. Click “Join Now” on the top right to sign up
-[https://lh5.googleusercontent.com/GXaCwe0N9-u6jfGLp1QNjAtbFstvihaBK-qeo9PYR_dJmoEfEoyq1IPeaWNPcLekl47qgZdeMar7iKbrpHsB0I25m4qcvTrhR2jczfNPz1U0kKylqI9hYR1c14zd9ALwwKYwiova](https://lh5.googleusercontent.com/GXaCwe0N9-u6jfGLp1QNjAtbFstvihaBK-qeo9PYR_dJmoEfEoyq1IPeaWNPcLekl47qgZdeMar7iKbrpHsB0I25m4qcvTrhR2jczfNPz1U0kKylqI9hYR1c14zd9ALwwKYwiova)
+[https://icon.vote/static/img8.png](https://icon.vote/static/img8.png)
 2. Type in “Username” and “Password” to sign up
 3. Verify ICX wallet ownership by signing a transaction from one’s wallet
     1. Click “Verify ICON Address” to sign a transaction
     2. Click “Check Your Verified Address” so that the blockchain can confirm
-[https://lh5.googleusercontent.com/d-DtJB4tbztPgrotVcNn1XCnziLblW8MNMA-UKZPDArJ6WUog6MjUXNyJZptdu6o2dGIVASI8CgutOBd20XpS5rXgirFLFgv_9GRR_liap6naniiFdmSYlXbQG4GRHaFUyF7tO5P](https://lh5.googleusercontent.com/d-DtJB4tbztPgrotVcNn1XCnziLblW8MNMA-UKZPDArJ6WUog6MjUXNyJZptdu6o2dGIVASI8CgutOBd20XpS5rXgirFLFgv_9GRR_liap6naniiFdmSYlXbQG4GRHaFUyF7tO5P)
+[https://icon.vote/static/img9.png](https://icon.vote/static/img9.png)
 5. Blockchain has verified the wallet ownership. The following image will appear
     1. Click “Make New Profile” to build your own profile
-[https://lh5.googleusercontent.com/t82GviB4OGbkr2YyUCdReFcmlJ42rP-wkBM90569aFofMlS82ZrKa3ROPUcg-H9nT1I6T-N83TNqa9jdzaYHIFkgPEamVaF884EsDUVJT-yPfmlpY3YcffDZOeZ20bS2_9mdJI8O](https://lh5.googleusercontent.com/t82GviB4OGbkr2YyUCdReFcmlJ42rP-wkBM90569aFofMlS82ZrKa3ROPUcg-H9nT1I6T-N83TNqa9jdzaYHIFkgPEamVaF884EsDUVJT-yPfmlpY3YcffDZOeZ20bS2_9mdJI8O)
+[https://icon.vote/static/img10.png](https://icon.vote/static/img10.png)
 
 Participate in a vote
 
@@ -82,7 +82,7 @@ Participate in a vote
     4. Total # of votes: # of P-rep delegated ICX that participated in the vote
     5. Your # of votes: # of user’s ICX delegated to the P-rep
     6. Votes will be recorded on the blockchain and shown at the bottom of the vote
-[https://lh3.googleusercontent.com/QUl66JtrB88Sq4YqvTkTPUAzeYxswX4wmMGHbQMU2kQrZtJZQpS2Hdn_Gfn_-Y7B-fuXAjAwnMB12u95yo1qSbUcwKQ2Y9t-WRX4XzGnBIT1qWmHjLaITiXEXZZ0qLFSE80yUIBu](https://lh3.googleusercontent.com/QUl66JtrB88Sq4YqvTkTPUAzeYxswX4wmMGHbQMU2kQrZtJZQpS2Hdn_Gfn_-Y7B-fuXAjAwnMB12u95yo1qSbUcwKQ2Y9t-WRX4XzGnBIT1qWmHjLaITiXEXZZ0qLFSE80yUIBu)
+[https://icon.vote/static/img11.png](https://icon.vote/static/img11.png)
 `;
 
   return (
